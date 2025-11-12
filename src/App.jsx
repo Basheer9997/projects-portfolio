0247e6315d6f026b658bbc5279b1a608465cbce5
Basheer <basheer714811@gmail.com>
2025-11-12 12:50:55 +0530

    added new projects to the profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Component} from 'react'
+import { Component } from 'react'
 
 import TabItem from './components/TabItem'
 import ProjectItem from './components/ProjectItem'
@@ -7,9 +7,10 @@ import Header from './components/Header'
 import './App.css'
 
 const tabsList = [
-  {tabId: 'STATIC', displayText: 'Static'},
-  {tabId: 'RESPONSIVE', displayText: 'Responsive'},
-  {tabId: 'DYNAMIC', displayText: 'Dynamic'},
+  { tabId: 'STATIC', displayText: 'Static' },
+  { tabId: 'RESPONSIVE', displayText: 'Responsive' },
+  { tabId: 'DYNAMIC', displayText: 'Dynamic' },
+  { tabId: 'REACT', displayText: 'React' },
 ]
 
 const projectsList = [
@@ -108,13 +109,112 @@ const projectsList = [
     githubUrl: 'https://github.com/Basheer9997/Sizing-an-Image.git',
     liveUrl: 'https://basheer9997.github.io/Sizing-an-Image/',
   },
+  
+  {
+    projectId: 11,
+    category: 'REACT',
+    imageURL: 'https://assets.ccbp.in/frontend/content/react-js/nxt-trendz-authentication-lg-home-output.png',
+    title: 'NxtTrendz',
+    description:
+      'A React-based shopping application (Nxt Trendz clone) with authentication, cart management, quantity controls, and dynamic cart summary using Context API.',
+    githubUrl: 'https://github.com/Basheer9997/nxt-trendz-online-shopping-application.git',
+    liveUrl: 'https://nxttrendzbasher.ccbp.tech/',
+  },
+  {
+    projectId: 12,
+    category: 'REACT',
+    imageURL: 'src/assets/images/image.png',
+    title: 'Restaurant App',
+    description:
+      'A dynamic React restaurant menu app with API integration, sliding tabs, dish customization, and interactive cart management.',
+    githubUrl: 'https://github.com/Basheer9997/Restaurant-App.git',
+    liveUrl: 'https://restaurantappbs.ccbp.tech/',
+  },
+  {
+    projectId: 13,
+    category: 'REACT',
+    imageURL: 'https://assets.ccbp.in/frontend/content/react-js/prime-video-lg-output-img.png',
+    title: 'Prime Video Clone',
+    description: 'A React-based Prime Video clone featuring movie sliders with React Slick and popup video playback using React Player.',
+    githubUrl: 'https://github.com/Basheer9997/prime-video-clone.git',
+    liveUrl: 'https://primevidclonebs.ccbp.tech/',
+  },
+  {
+    projectId: 14,
+    category: 'REACT',
+    imageURL: 'https://assets.ccbp.in/frontend/content/react-js/jobby-app-home-lg-output.png',
+    title: 'Jobby App',
+    description:
+      'A React-based job search app enabling users to log in, filter, and explore job listings with integrated API and state management.',
+    githubUrl: 'https://github.com/Basheer9997/jobby-app.git',
+    liveUrl: 'https://jobbyappbs2.ccbp.tech/',
+  },
+  {
+    projectId: 15,
+    category: 'REACT',
+    imageURL: 'https://assets.ccbp.in/frontend/content/react-js/match-game-lg-output.png',
+    title: 'React Match Game',
+    description: 'An interactive React image-matching game with a 60-second timer, category filters, score tracking, and a “Play Again” option.',
+    githubUrl: 'https://github.com/Basheer9997/react-match-game.git',
+    liveUrl: 'https://matchgamebs.ccbp.tech/',
+  },
+  {
+    projectId: 16,
+    category: 'REACT',
+    imageURL: 'https://assets.ccbp.in/frontend/content/react-js/github-repos-lg-success-output.png',
+    title: 'GitHub Popular Repos',
+    description: 'A React app that fetches and displays popular GitHub repositories by language, with filtering and detailed repository insights.',
+    githubUrl: 'https://github.com/Basheer9997/Github-Popular-Repos.git',
+    liveUrl: 'https://ghpoplrreposbs.ccbp.tech/',
+  },
+  {
+    projectId: 17,
+    category: 'REACT',
+    imageURL: 'https://assets.ccbp.in/frontend/content/react-js/simple-todos-lg-output-v2.png',
+    title: 'Simple Todos',
+    description:
+      'A React-based Todo List app with add, edit, complete, and batch-add functionalities, featuring a clean and interactive UI.',
+    githubUrl: 'https://github.com/Basheer9997/Simple-Todos.git',
+    liveUrl: 'https://simpletodosbs.ccbp.tech/',
+  },
+  
+  
+  {
+    projectId: 18,
+    category: 'REACT',
+    imageURL: 'https://assets.ccbp.in/frontend/content/react-js/ipl-dashboard-home-lg-output.png',
+    title: 'IPL Dashboard App',
+    description: 'A React application that displays IPL team details, latest matches, and recent matches with dynamic routing and API integration.',
+    githubUrl: 'https://github.com/Basheer9997/IPL-Dashboard-App.git',
+    liveUrl: 'https://ipldbbs.ccbp.tech/',
+  },
+  {
+    projectId: 19,
+    category: 'REACT',
+    imageURL: 'https://assets.ccbp.in/frontend/content/react-js/app-store-lg-output-v2.png',
+    title: 'React App Store',
+    description: 'A React-based App Store interface with tab navigation and case-insensitive search for browsing and filtering apps by category.',
+    githubUrl: 'https://github.com/Basheer9997/react-app-store.git',
+    liveUrl: 'https://basheerappstore.ccbp.tech/',
+  },
+  
+  {
+    projectId: 20,
+    category: 'REACT',
+    imageURL: 'https://assets.ccbp.in/frontend/content/react-js/emoji-game-lg-output.png',
+    title: 'Emoji Game',
+    description: 'An interactive React Emoji Game where players click unique emojis to score, track top scores, and avoid repeats to stay in the game.',
+    githubUrl: 'https://github.com/Basheer9997/Emoji-Game.git',
+    liveUrl: 'https://emojigamebs.ccbp.tech/',
+  },
+  
 ]
 
 class App extends Component {
-  state = {activeTabId: tabsList[0].tabId}
+  state = { activeTabId: tabsList[0].tabId }
 
   getFilterdProjects = () => {
-    const {activeTabId} = this.state
+    const { activeTabId } = this.state
     const filteredProjects = projectsList.filter(
       eachProjectDetails => eachProjectDetails.category === activeTabId,
     )
@@ -122,12 +222,12 @@ class App extends Component {
   }
 
   updateActiveTabId = tabId => {
-    this.setState({activeTabId: tabId})
+    this.setState({ activeTabId: tabId })
   }
 
   render() {
     const filteredProjects = this.getFilterdProjects()
-    const {activeTabId} = this.state
+    const { activeTabId } = this.state
     return (
       <div className="app-container">
         <Header />
